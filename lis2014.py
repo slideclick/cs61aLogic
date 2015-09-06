f69b8041da9b4588cef08f7ddcdc4c3552da44a8
unknown <lius@CPEKW-Q1100534.is.ad.igt.com>
2015-09-06 12:35:53 +0800

    adding @trace. 当解释到一个lambda时，知道，那是个func，是要稍后执行的东东。这个东东要存下来，这个东东在解释时，我要new个东西，这个东西是Procedure的实例。它解释时，也即是__call__时，再取出来new时记录的状态。

--- lis2014.py
+++ lis2014.py
@@ -9,7 +9,48 @@
 3628800
 '''
 from __future__ import division
+import re
+###############################
 
+
+
+
+
+###############################
+PREFIX = ''
+def trace(fn):
+    """A decorator that prints a function's name, its arguments, and its return
+    values each time the function is called. For example,
+
+    @trace
+    def compute_something(x, y):
+        # function body
+    """
+    @functools.wraps(fn)
+    def wrapped(*args, **kwds):
+        global PREFIX
+        reprs = [repr(e) for e in args] 
+        reprs += [repr(k) + '=' + repr(v) for k, v in kwds.items()]
+        log('{0}({1})'.format(fn.__name__, ', '.join(reprs)) + ':')
+        PREFIX += '    '
+        try:
+            result = fn(*args, **kwds)
+            PREFIX = PREFIX[:-4]
+        except Exception as e:
+            log(fn.__name__ + ' exited via exception')
+            PREFIX = PREFIX[:-4]
+            raise
+        # Here, print out the return value.
+        log('{0}({1}) -> {2}'.format(fn.__name__, ', '.join(reprs), result))
+        return result
+    return wrapped
+
+def log(message):
+    """Print an indented message (used with trace)."""
+    if type(message) is not str:
+        message = str(message)
+    print(PREFIX + re.sub('\n', '\n' + PREFIX, message))
+    
 Symbol = str          # A Lisp Symbol is implemented as a Python str
 List   = list         # A Lisp List is implemented as a Python list
 Number = (int, float) # A Lisp Number is implemented as a Python int or float
